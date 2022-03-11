e7a75dfae2cc8508750d84f18867cbc1cb23ecd2
Tobias Kunze <r@rixx.de>
2022-03-11 23:36:19 +0100

    Add simple stave windows

--- drafts/cathedral/sketch_2022_01_26_windows.py
+++ drafts/cathedral/sketch_2022_01_26_windows.py
@@ -9,6 +9,9 @@ class Vsk:
     def rand_int(self, minimum, maximum):
         return int(self.v.random(minimum, maximum + 1))
 
+    def coin_toss(self):
+        return bool(self.rand_int(0, 1))
+
 def rotate_point(x, y, degrees, origin=None):
     rotate_x, rotate_y = origin if origin else (0, 0)
     if rotate_x != 0:
@@ -127,11 +130,169 @@ class RoseWindow(Vsk):
             sketch.rotate(segment_rotation, degrees=True)
         sketch.translate(-x, -y)
 
+class StaveWindow(Vsk):
+    CIRCLE_DECORATION_COUNT = 6
+
+    def __init__(self, *args, width=None, height=None, center=None):
+        super().__init__(*args)
+        self.width = width or 200
+        self.height = height or width * self.rand_int(22, 30) / 10
+        self.center = center or (0, 0)
+        self.stave_count = self.rand_int(2, 4)
+        self.divided_staves = self.coin_toss()
+        self.stave_distance = self.width * 0.04
+
+    def draw_stave(self, x, y, width, height, arch_height=None, sub_staves=0, sub_stave_config=None, stave_distance=None, decoration=False):
+        sub_stave_config = sub_stave_config or {}
+        stave_distance = stave_distance or self.stave_distance
+        arch_height = arch_height or width * 0.8
+        lower_height = height - arch_height
+
+        self.v.line(x, y + arch_height, x, y + height)
+        self.v.line(x, y + height, x + width, y + height)
+        self.v.line(x + width, y + height, x + width, y + arch_height)
+
+        arch_center = (x + width / 2, y)
+        anchor_height = y + arch_height * 0.75
+
+        lower_anchor = arch_height * 0.35
+        upper_anchor_width = width * 0.25
+        upper_anchor_height = arch_height * 0.1
+
+        self.v.bezier(
+            x, y + arch_height,
+            x, y + arch_height - lower_anchor,
+            arch_center[0] - upper_anchor_width, arch_center[1] + upper_anchor_height,
+            arch_center[0], arch_center[1],
+        )
+        self.v.bezier(
+            x + width, y + arch_height,
+            x + width, y + arch_height - lower_anchor,
+            arch_center[0] + upper_anchor_width, arch_center[1] + upper_anchor_height,
+            arch_center[0], arch_center[1],
+        )
+
+        if sub_staves:
+            stave_width = (width - (stave_distance * (sub_staves + 1))) / sub_staves
+            stave_base_height = height - arch_height
+            stave_arch_height = arch_height / sub_staves
+            stave_height = stave_base_height + stave_arch_height
+            sub_stave_y = y + height - stave_distance - stave_height
+            circle_width = width * 0.8 / sub_staves
+            decoration_type = self.rand_int(1, self.CIRCLE_DECORATION_COUNT)
+            for stave in range(sub_staves):
+                sub_stave_x = x + stave * (stave_width + stave_distance) + stave_distance
+                self.draw_stave(
+                    sub_stave_x,
+                    sub_stave_y,
+                    stave_width,
+                    stave_height,
+                    stave_distance=stave_distance / 2,
+                    arch_height=stave_arch_height,
+                    **sub_stave_config,
+                )
+
+            circle_y = y + arch_height - stave_arch_height * 1.35
+            for row in range(sub_staves - 1):
+                for circle in range(sub_staves - 1 - row):
+                    circle_x = x + (1 + circle + 0.5 * row) * (stave_width + stave_distance) + stave_distance / 2
+                    self.draw_circle(
+                        circle_x,
+                        circle_y,
+                        circle_width,
+                        decoration_type=decoration_type,
+                    )
+                circle_y -= circle_width
+
+    def draw_circle(self, x, y, width, decoration_type=None):
+        self.v.circle(x, y, width)
+        decoration_type = decoration_type or self.rand_int(1, self.CIRCLE_DECORATION_COUNT)
+        decoration_type = 1
+        if decoration_type == 1:  # Three circles
+            circle_width = math.sqrt((width*0.85) ** 2)  # TODO why
+            degrees = [30, 150, 270]
+            triangle_points = [
+                (x + width / 2 * math.cos(d * math.pi / 180), y + width / 2 * math.sin(d * math.pi / 180))
+                for d in degrees
+            ]
+            midpoints = [
+                midpoint(*triangle_points[0], *triangle_points[1]),
+                midpoint(*triangle_points[1], *triangle_points[2]),
+                midpoint(*triangle_points[2], *triangle_points[0]),
+
+            ]
+            self.v.arc(
+                *midpoints[0],
+                circle_width, circle_width,
+                0, 60,
+                degrees=True,
+            )
+            self.v.arc(
+                *midpoints[0],
+                circle_width, circle_width,
+                120, 180,
+                degrees=True,
+            )
+            self.v.arc(
+                *midpoints[1],
+                circle_width, circle_width,
+                0, 60,
+                degrees=True,
+            )
+            self.v.arc(
+                *midpoints[1],
+                circle_width, circle_width,
+                240, 300,
+                degrees=True,
+            )
+            self.v.arc(
+                *midpoints[2],
+                circle_width, circle_width,
+                240, 300,
+                degrees=True,
+            )
+            self.v.arc(
+                *midpoints[2],
+                circle_width, circle_width,
+                120, 180,
+                degrees=True,
+            )
+
+        if decoration_type == 2:  # Four circles
+            pass
+        if decoration_type == 3:  # Five circles
+            pass
+        if decoration_type == 4:  # Six circles
+            pass
+
+    def draw(self):
+        upper_right = (self.center[0] - self.width / 2, self.center[1])
+        self.draw_stave(
+            *upper_right,
+            self.width, self.height,
+            arch_height = self.width * 0.8,
+            sub_staves=self.stave_count,
+            sub_stave_config={
+                "sub_staves": 2 if self.divided_staves else 0,
+                "decoration": False if self.divided_staves else True,
+                "sub_stave_config": {
+                    "decoration": True,
+                }
+            },
+        )
+
+        stave_height = self.height - self.width * 0.5
+
+
 class Cathedral(Vsk):
     def draw(self):
-        center_width = self.rand_int(150, 250)
-        rose_window = RoseWindow(self.v, width=center_width - 20, center=(10,10))
-        rose_window.draw()
+        # center_width = self.rand_int(150, 250)
+        # rose_window = RoseWindow(self.v, width=center_width - 20, center=(10,10))
+        # rose_window.draw()
+        center_width = 300
+        stave_window = StaveWindow(self.v, width=center_width - 20, center=(10,10))
+        stave_window.draw()
+
 
 
 class WindowsSketch(vsketch.SketchClass):
